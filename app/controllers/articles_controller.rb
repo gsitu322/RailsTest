da572aa34e156d313b3da78d5d31cb5322b1dd16
gsitu322 <gsitu322@gmail.com>
2017-06-17 23:45:15 -0400

    Consolidated forms and added delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,13 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
-    
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article Was Successfully Deleted"
+    redirect_to articles_path
   end
   
   private
